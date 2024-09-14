f2a6d31c17cfadeb8bb23e03abe75f4e1a43e843
Jay Padmani <jay.padmani@evital.in>
2024-09-14 10:11:38 +0530

    done

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
     console.log(" Now it is working  ");
+    console.log(" It is Done.");
 })
